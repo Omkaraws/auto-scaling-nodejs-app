0c20732c4f2299d4ecdb898b4f48db9d241e38bc
Omkaraws <113175832+Omkaraws@users.noreply.github.com>
2023-11-28 00:37:13 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,24 +1,11 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const port = 3000;
 
-/**
- * GET /hello
- * Returns "hello World!" 
- */
-app.get('/hello', (_req, res) => {
-  res.send('Hello World!')
+app.get('/', (req, res) => {
+  res.send('<h1>Express Demo App</h1> <h4>Message: Success</h4> <p>Version 1.1</p>');
 })
 
-/**
- * GET /wait
- * Waits for five seconds and then returns OK 
- */
-app.get('/wait', (_req, res) => {
-  // wait for five seconds then return ok
-  setTimeout(() => {
-    res.send('ok')
-  }
-  , 5000)
+app.listen(port, ()=> {
+  console.log(`Demo app is up and listening to port: ${port}`);
 })
-
-app.listen(3000, () => console.log('Listening on http://localhost:3000'))
